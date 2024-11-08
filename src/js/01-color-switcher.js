9930ea6bb44a783f26cf0501f7c05afe5cc01723
Danik2023 <danik.kyrteu2@gmail.com>
2024-11-08 16:45:16 +0200

    added functionality to the ex1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let timer = {};
 
+startBtn.addEventListener('click', () => {
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
